c7dd443d39ac293e0a45df403e5f7bbd66a8fbf0
BrunoMartins11 <brunocmartins@live.com.pt>
2018-05-09 14:05:40 +0100

    retifica parser e altera o read line

--- Projeto/parser.c
+++ Projeto/parser.c
@@ -2,33 +2,25 @@
 
 COMMAND parser(char* buff, COMMAND cm){
 
-	for(int i=0; buff; i++){
 		if(buff[i] == '$' ){
 			if(!cm)
 				cm = make_command(buff,NULL,NULL);
-				break;
 				else{ 
 					set_command_input(cm,buff); 
-					break;
 					}
 		}
-		if(buff[i]!=' ' && buff[i]!='$' && buff[i]!='>>>'){
+		if(buff[i]!='$' && strncmp(buff,">>>",3)){
 		    if(!cm)
 				cm = make_command(NULL,NULL,buff);
-				break;
 				else{ 
 					set_command_comment(cm,buff); 
-					break;
 					}	
 		}
-		if(buff[i]=='>>>'){
+		if(strncmp(buff,">>>",3)){
 		    if(!cm)
 				cm = make_command(NULL,buff,NULL);
-				break;
 				else{ 
-					set_command_output(cm,buff); 
-					break;
+					set_command_output(cm,buff);
 					}
-	}
 	return cm;
 }
